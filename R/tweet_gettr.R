46e167fffaf95ec1cca6900856511e4a7dec5229
Bryan Martin <bmartin6@uw.edu>
2020-01-31 11:58:54 -0800

    Add additional parameters to tweet_gettr

--- R/tweet_gettr.R
+++ R/tweet_gettr.R
@@ -3,28 +3,33 @@
 #' This function lets you scrape the timeline of any twitter user. You MUST run the setup function before using this function in order for it to work.
 #' @param handle The handle of the twitter user's timeline you wish to scrape.
 #' @param output The location of an output text file that you can re-use if you want a re-usable file of tweets. Defaults to a blank string, no output saved.
+#' @param n Integer. Defaults to `3200`. The number of tweets to pull.
+#' @param includeRts Boolean. Defaults to `FALSE`. Whether or not to include re-tweets.
+#' @param excludeReplies Boolean. Defaults to `TRUE`. Wehter or not to exclude replies.
 #' @keywords Twitter, Scraping
 #' @return Returns a properly-formatted list that you can run make_sentence on
 #' @export
 #' @examples
 #' test_function("@realDonaldTrump", "./trump.txt")
 
-tweet_gettr <- function(handle, output = "") {
+tweet_gettr <- function(handle, output = "", n = 3200,
+                        includeRts = FALSE, excludeReplies = TRUE) {
   # Make sure that this is an @
   if (substr(handle,1,1) != '@') {
     handle <- paste('@', handle, sep = "")
   }
-  
+
   # Scrape handle's timeline.
   #  Exits if the twitter api is not setup
   tweets <- tryCatch(
-    {twitteR::userTimeline(handle, n= 3200, includeRts = TRUE, excludeReplies = FALSE)},
-    error=function(cond) {
+    {twitteR::userTimeline(handle, n = n, includeRts = includeRts,
+                           excludeReplies = excludeReplies)},
+    error = function(cond) {
       message("Error: Twitter api not setup. Please use setup_twitteR in order to scrape tweets from Twitter")
       stop()
     }
   )
-  
+
   tweets <- twitteR::twListToDF(tweets)
   tweets <- tweets$text
   tweets <- stringr::str_replace_all(tweets, "'", "")
