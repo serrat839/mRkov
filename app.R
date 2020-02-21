17fc8bffd09b69da8445406292e6fed6ff5adde5
Thomas Serrano <serrat@uw.edu>
2020-02-20 20:31:10 -0800

    Create the skeleton of a shiny app

---
+++ app.R
@@ -0,0 +1,7 @@
+source("my_ui.R")
+source("my_server.R")
+
+mRkov::setup_twitteR()
+# To start running your app, pass the variables defined in previous
+# code snippets into the `shinyApp()` function
+shinyApp(ui = my_ui, server = my_server)
