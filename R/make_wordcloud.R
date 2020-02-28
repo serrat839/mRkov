0fa41da85546d101faead72fa2eeb1102cc91c5e
Thomas Serrano <serrat@uw.edu>
2020-02-27 22:09:56 -0800

    Add option to use n words in wordcloud

--- R/make_wordcloud.R
+++ R/make_wordcloud.R
@@ -5,13 +5,14 @@
 #'
 #' @param information List. The output from tweet_gettr or read_text_file
 #' @param stops String or vector of strings. Words to remove from the word cloud
+#' @param n Int. Max number of words to be included in wordcloud. Defaults to Inf.
 #' @keywords setup, api
 #' @export
 #' @examples
 #' make_wordcloud(trump_tweets)
 #'
 
-make_wordcloud <- function(information, stops = "") {
+make_wordcloud <- function(information, stops = "", n = Inf) {
   if (is.null(information)) {
     return(NULL)
   }
@@ -24,5 +25,5 @@ make_wordcloud <- function(information, stops = "") {
   stop_words <- c(stop_words, stops, "endofline")
   stop_words <- data.frame(stop_words, stringsAsFactors = F)
   information <- dplyr::anti_join(information, stop_words, by= c("lowercase_tokens" = "stop_words"))
-  wordcloud::wordcloud(information$lowercase_tokens, information$n)
+  wordcloud::wordcloud(information$lowercase_tokens, information$n, max.words = n)
 }
