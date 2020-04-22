80fe0c8651d266e6090a27b06222412e38c937ce
Thomas Serrano <serrat@uw.edu>
2020-04-21 18:05:29 -0700

    Add mRkov.R to document the package. Fixed #25

---
+++ R/mRkov.R
@@ -0,0 +1,12 @@
+#' Corncob package documentation.
+#'
+#' Corncob provides methods for estimating and plotting count data. Specifically, corncob is designed to account for the challenges of modeling sequencing data from microbial abundance studies.
+#'
+#'
+#' @name mRkov-package
+#' @aliases mRkov
+#' @details
+#' mRkov is an R package that allows users to quickly parse textual data to be simulated using markov chain monte carlo methods.
+#'
+#' The development version of the package will be maintained on \url{https://github.com/serrat839/mRkov}.
+NULL
