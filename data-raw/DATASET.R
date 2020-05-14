2fcd55a2bf028c647f1f91dfb85ad060ec318c2f
Thomas Serrano <serrat@uw.edu>
2020-05-14 13:08:14 -0700

    Remove dataset 'trump tweets'

--- data-raw/DATASET.R
+++
@@ -1,4 +0,0 @@
-## code to prepare `DATASET` dataset goes here
-library(mRkov)
-trump_tweets <- tweet_gettr("@realdonaldtrump", includeRts = FALSE, includeReplies = T)
-usethis::use_data(trump_tweets)
